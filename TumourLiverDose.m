cfe67ea4bd854367f71569358eb9bd7c88bffc06
beeno <ouyang.ben@gmail.com>
2020-04-05 19:25:56 -0400

    Update TumourLiverDose.m

--- TumourLiverDose.m
+++ TumourLiverDose.m
@@ -1,4 +1,4 @@
-function dNPdt = TumourLiverDoseRates(t,y,flag,kT,k_T,kL,kI,k_L,kO,k_O,k_I,L0,T0,O0,I0)
+function dNPdt = TumourLiverDose(t,y,flag,kT,k_T,kL,kI,k_L,kO,k_O,k_I,L0,T0,O0,I0)
     dNPdt = [-kT*y(1)*(T0-y(3)) - kL*y(1)*(L0-y(2)) - kO*y(1)*(O0-y(4));
              + kL*y(1)*(L0-y(2)) - kI*y(2)*(I0-y(5));
              + kT*y(1)*(T0-y(3));
